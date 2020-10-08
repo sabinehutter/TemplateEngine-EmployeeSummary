0e0ed3c5aa7e0486950bd1aeb9c92d5bd2d2e307
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-10-08 00:29:42 -0400

    initial function layout

--- Develop/app.js
+++ Develop/app.js
@@ -1,6 +1,6 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const ManagerExport = require("./lib/Manager");
+const EngineerExport = require("./lib/Engineer");
+const InternExport = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -9,6 +9,72 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+class Employee {
+    constructor (name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName(){
+    }
+
+    getId(){
+
+    }
+
+    getEmail(){
+
+    }
+    getRole(){
+        // returns Employee
+    }
+}
+
+class Manager extends Employee {
+    constructor (name, id, email,officeNumber){
+        super(name, id, email)
+        this.officeNumber = officeNumber;
+        
+    }
+
+    getRole(){
+// Overridden to return Manager
+    }
+}
+
+class Engineer extends Employee {
+constructor (name, id, email, github){
+        super(name, id, email)
+        this.github = github;
+        
+    }
+
+    getGithub(){
+        
+    }
+
+    getRole(){
+        // Overridden to return Engineer
+    }
+}
+
+class Intern extends Employee {
+    constructor (name, id, email, school){
+        super(name, id, email)
+        this.school = school;
+        
+    }
+
+    getSchool(){
+        
+    }
+
+    getRole(){
+        // Overridden to return Intern
+    }
+}
 
 
 // Write code to use inquirer to gather information about the development team members,
